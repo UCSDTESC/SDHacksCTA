285d80cfb2f1d9d49ba5d111044375cfdf68dbfb
Subhankar Panda <s1panda@ucsd.edu>
2018-04-29 00:22:51 -0700

    Added MailChimp Mailing List

---
+++ js/sdhacks-cta.js
@@ -0,0 +1,8 @@
+$('#cta-btn').click((e) => {
+  e.preventDefault();
+  $('#mc_embed_signup').fadeToggle("fast", () => {}); 
+});
+
+/*$('#mc-embedded-subscribe').click((e) => {
+  $('#mc_embed_signup').fadeToggle("slow", () => {}); 
+})*/
