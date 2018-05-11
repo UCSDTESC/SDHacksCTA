1fe2446a72ff23794fa953c0543b463b18e72f42
Subhankar Panda <s1panda@ucsd.edu>
2018-05-11 13:27:11 -0700

    Added Date and Location

--- js/sdhacks-cta.js
+++
@@ -1,8 +0,0 @@
-$('#cta-btn').click((e) => {
-  e.preventDefault();
-  $('#mc_embed_signup').fadeToggle("fast", () => {}); 
-});
-
-/*$('#mc-embedded-subscribe').click((e) => {
-  $('#mc_embed_signup').fadeToggle("slow", () => {}); 
-})*/
